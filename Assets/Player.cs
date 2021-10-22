c9a7da2b88060fa38f6e95e0f47737d605ba298d
Vitor <vitor@MacBook-Pro-de-Vitor.local>
2021-10-22 14:34:18 -0300

    Mira implementada

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 1;
+    public Transform crosshair;
     
     private void Start()
     {
@@ -11,7 +12,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovimentaJogador();
+                
+    }
+
+    private void MovimentaJogador()
+    {
         var movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         transform.position = transform.position + movement * speed * Time.deltaTime;
+
+        var mouse = Input.mousePosition;
+        crosshair.position = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, 10));
     }
 }
